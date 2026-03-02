58983325eaa9891b7e49772f7b24680e2ee37ecd
OwamMapolisa2006 <4535074@myuwc.ac.za>
2026-03-02 14:08:35 +0000

    initialising the data

--- Practical14.java
+++ Practical14.java
@@ -1,3 +1,35 @@
- public class Practical14{
+import java.util.*;
+public class Practical14{
    
+
+    static int N = 1 << 20; // 2^20 = 1,048,576
+    static int MAX_DATA = 950000;
+    static int REPETITIONS = 30;
+
+    public static void main(String[] args) {
+
+        Pair[] data = generateData();
+
+        double[] loadFactors = {0.75, 0.80, 0.85, 0.90, 0.95};
+
+        System.out.println("Average Time (seconds)");
+        System.out.println("Load\tOpenHash\tChainedHash");
+        System.out.println("---------------------------------------");
+
+        for (double alpha : loadFactors) {
+
+            int nEntries = (int)(MAX_DATA * alpha / 0.95);
+            int m = (int)(nEntries / alpha);
+
+            double openTime = timeOpenHash(data, nEntries, m);
+            double chainedTime = timeChainedHash(data, nEntries, m);
+
+            System.out.printf("%.0f%%\t%.6f\t%.6f\n",
+                    alpha * 100,
+                    openTime,
+                    chainedTime);
+        }
+        
+    }
+
  }
