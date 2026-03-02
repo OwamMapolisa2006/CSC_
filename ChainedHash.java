6a4b5ec84ec743eaa3158c7a7dd4c52df57b9d52
OwamMapolisa2006 <4535074@myuwc.ac.za>
2026-03-02 13:57:14 +0000

    done with chained hash

--- ChainedHash.java
+++ ChainedHash.java
@@ -1,3 +1,57 @@
+ class Node {
+    String key;
+    String value;
+    Node next;
+
+    Node(String k, String v) {
+        key = k;
+        value = v;
+        next = null;
+    }
+}
+
 public class ChainedHash {
-    
+
+    private Node[] table;
+    private int m;
+
+    public ChainedHash(int m) {
+        this.m = m;
+        table = new Node[m + 1];
+    }
+     private int hash(String key) {
+        return Math.abs(key.hashCode()) % m + 1;
+    }
+
+    public void insert(String key, String value) {
+
+        int i = hash(key);
+        Node current = table[i];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                current.value = value;
+                return;
+            }
+            current = current.next;
+        }
+
+        Node newNode = new Node(key, value);
+        newNode.next = table[i];
+        table[i] = newNode;
+    }
+
+    public String lookup(String key) {
+
+        int i = hash(key);
+        Node current = table[i];
+
+        while (current != null) {
+            if (current.key.equals(key))
+                return current.value;
+            current = current.next;
+        }
+
+        return null;
+    }
 }
