ef17c5de4203af9023364561231eb65c92787e0c
OwamMapolisa2006 <4535074@myuwc.ac.za>
2026-03-02 13:34:11 +0000

    Creating an insert key and value method

--- Openhash.java
+++ Openhash.java
@@ -10,5 +10,15 @@ public class Openhash {
         values = new String[m + 1];
         size = 0;
     }
-    
+     private String[] keys;
+    private String[] values;
+    private int m;
+    private int size;
+
+    public OpenHash(int m) {
+        this.m = m;
+        keys = new String[m + 1];   // indices 1..m
+        values = new String[m + 1];
+        size = 0;
+    }
 }
