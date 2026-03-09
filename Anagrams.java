96015410a1e7ea49283d2a85493d8c528edc96dc
OwamMapolisa2006 <4535074@myuwc.ac.za>
2026-03-09 13:01:31 +0000

    introducing hash map

--- Anagrams.java
+++ Anagrams.java
@@ -6,6 +6,15 @@ public class Anagrams{
             System.out.println("Usage: java input file");
             return;
         }
+        String inputfile = args[0];
+        System.out.println("Data file: " + inputfile);
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(inputfile), "latin1"));
+        Map<String, Integer> D = new HashMap<>();
+
+        String line;
+        int linenumber = 0;
        } 
     }
 }
