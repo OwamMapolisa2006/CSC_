0c22643e6994c9d74e30dec9423aa1ef60e46acc
OwamMapolisa2006 <4535074@myuwc.ac.za>
2026-03-09 12:53:46 +0000

    trying to sort the angrams

---
+++ Anagrams.java
@@ -0,0 +1,11 @@
+public class Anagrams{
+    public static void main(String[]args){
+       char[] char=word.toCharArray();
+       throws Exception{
+        if(args!=1){
+            System.out.println("Usage: java input file");
+            return;
+        }
+       } 
+    }
+}
