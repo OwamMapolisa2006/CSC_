aac5d08357969f4a678710cb631a9714568c855b
OwamMapolisa2006 <4535074@myuwc.ac.za>
2026-03-09 13:18:14 +0000

    Printing anagrams

--- Anagrams.java
+++ Anagrams.java
@@ -33,6 +33,44 @@ public class Anagrams{
         }
 
         reader.close();
+         Map<String, List<String>> A = new HashMap<>();
+
+        for (String w : D.keySet()) {
+
+            String a = signature(w);
+
+            if (!A.containsKey(a)) {
+                A.put(a, new ArrayList<>());
+            }
+
+            A.get(a).add(w);
+        }
+
+        PrintWriter out = new PrintWriter("anagrams");
+
+        for (String key : A.keySet()) {
+
+            List<String> list = A.get(key);
+
+            if (list.size() > 1) {
+
+                String anagramlist = String.join(" ", list);
+
+                out.println(anagramlist + "\\\\");
+
+                for (int i = 0; i < list.size() - 1; i++) {
+
+                    int space = anagramlist.indexOf(" ");
+
+                    anagramlist =
+                            anagramlist.substring(space + 1) + " " +
+                            anagramlist.substring(0, space);
+
+                    out.println(anagramlist + "\\\\");
+                }
+            }
+        }
+
        } 
     }
 }
