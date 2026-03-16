c33977685e24f65b0f15c829d6f1ae11c8957c8d
OwamMapolisa2006 <4535074@myuwc.ac.za>
2026-03-16 13:21:17 +0000

    Start heapsort project

--- tryHeapsort.java
+++ tryHeapsort.java
@@ -1,10 +1,26 @@
-import java.util.bufferedReader;
+import java.util.BufferedReader;
 import java.util.FileReader;
 
 public class tryHeapsort{
-    public static void main(String[]args){
-        arr[] words= {};
-        String temp;
-        
+     static void swap(String[] arr, int i, int j){
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static void heapify(String[] arr, int n, int i){
+        int largest = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+
+        if(left < n && arr[left].compareTo(arr[largest]) > 0){
+            largest = left;
+        }
+        if(right < n && arr[right].compareTo(arr[largest]) > 0){
+            largest = right;
+        }
+        if(largest != i){
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
     }
 }
